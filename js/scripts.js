7a6ba775110c991aca8fcad6eb17bffb80742f55
jg <stratacast@comcast.net>
2019-06-07 09:11:44 -0700

    add function using jquery to make the survey functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+  $("#survey form").submit(function(event) {
+    var question1 = $("input:radio[name=question1]:checked").val();
+    var result;
+    if (question1 === "option1") {
+      result = $("#answer1").show();
+    } else if (question1 === "option2") {
+      result = $("#answer2").show();
+    } else if (question1 === "option3") {
+      result = $("#answer3").show();
+    }
 
+
+    event.preventDefault();
+  });
+});
